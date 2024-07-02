d491f9063ccafb939ef4093da556b4840ad87c76
Moumouni BELEM <moumouni.belem.ing@gmail.com>
2024-07-02 12:10:45 +0200

    feat: main class for monitoring

---
+++ facebook_page_monitor/facebook_page_monitor.py
@@ -0,0 +1,60 @@
+import facebook
+
+#This token does not work. It was my used for tesing but I modified it and lect it here so get an idea how it should look like
+page_access_token = 'EAAGCrYHozmYBOZCFWjGPCgWgoySgtWgzDTo7l3fpRWiLVQhmNk9TKmm2WWdUdZCoykc1GzaJsaLHfsMVM5Zodhgt5oDxabuIYDgBpWofSw2Abr89DKMGBZAOYNOzeF4GwEtXACcn7GZAAdJZArClsJn0ifQfgGvTMUS74h5vWLWeRg5ZBl3QbAkAP7wHOh4OFbLDneCdq5Uus9JjtTVZCIGSTZBsbucANmfs40LPfNp8ZD'
+
+class FacebookPageMonitor:
+
+    def __init__(self, page_access_token) -> None:
+
+        self.graph = facebook.GraphAPI(page_access_token)
+
+    
+    def post_message(self, page_id, msg):
+        self.graph.put_object(parent_object=page_id, connection_name='feed', message=msg)
+
+    def comment_post(self, post_id, msg):
+        self.graph.put_comment(object_id=post_id, message=msg)
+
+    def like_post(self, post_id):
+        self.graph.put_like(object_id=post_id)
+
+    def uplaod_photo(self, photo_path):
+        with open(photo_path, 'rb') as photo:
+            self.graph.put_photo(photo)
+
+    def fetch_page_ids(self, user):
+
+        pages = self.graph.get_object(user)
+
+        return pages
+    
+    def fetch_posts_ids(self):
+        posts = self.graph.get_connections(id='me', connection_name='posts')
+
+        return posts
+
+
+def main():
+    monitor = FacebookPageMonitor(page_access_token)
+
+    page_id = monitor.fetch_page_ids('me')['id']
+    
+    message = 'There we go, I am posting from a python script to be found on Github'
+    monitor.post_message(page_id, message)
+
+    post_id = monitor.fetch_posts_ids()['data'][0]['id'] # JUst to get the latest post
+    comment_message = 'A comment from again my python script!\n Life is Great'
+    monitor.comment_post(post_id, comment_message)
+
+    monitor.like_post(post_id)
+
+    photo_path = 'data/image.png'
+
+    monitor.uplaod_photo(photo_path)
+
+
+if __name__=='__main__':
+    main()
+
+    
